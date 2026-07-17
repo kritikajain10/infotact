58cc22daefc9d65c496c2b0c1eba1569a323a921
kritikajain10 <kritikajain10@gmail.com>
2026-07-18 00:01:38 +0530

    week 2 complete

--- frontend/src/components/StatusCard.tsx
+++ frontend/src/components/StatusCard.tsx
@@ -6,17 +6,37 @@ type Props = {
   function StatusCard({ title, value }: Props) {
     return (
       <div
-        style={{
-          background: "#1f2937",
-          color: "white",
-          padding: "20px",
-          borderRadius: "10px",
-          width: "180px",
-          textAlign: "center",
-        }}
+      style={{
+        background: "#1e293b",
+        color: "white",
+        padding: "20px",
+        borderRadius: "15px",
+        width: "180px",
+        textAlign: "center",
+        boxShadow: "0px 4px 12px rgba(0,0,0,0.3)",
+        transition: "0.3s ease",
+        cursor: "pointer",
+      }}
       >
-        <h3>{title}</h3>
-        <h1>{value}</h1>
+        <h3
+  style={{
+    fontSize: "18px",
+    fontWeight: "600",
+    marginBottom: "10px",
+  }}
+>
+  {title}
+</h3>
+
+<h1
+  style={{
+    fontSize: "42px",
+    fontWeight: "bold",
+    margin: 0,
+  }}
+>
+  {value}
+</h1>
       </div>
     );
   }
