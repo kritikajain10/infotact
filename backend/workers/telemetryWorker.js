a478503107b0f7b83a6c19e666f662c9694dae39
kritikajain10 <kritikajain10@gmail.com>
2026-07-21 23:10:26 +0530

    Integrate geofence check into telemetry worker

--- backend/workers/telemetryWorker.js
+++ backend/workers/telemetryWorker.js
@@ -1,8 +1,15 @@
 const { parentPort } = require("worker_threads");
+const { isInsideGeofence } = require(". ./utils/geofence");
 
 parentPort.on("message", (data) => {
+  const insideGeofence = isInsideGeofence(
+    data.longitude,
+    data.latitude
+  );
+
   const processed = {
     ...data,
+    insideGeofence,
     processedAt: new Date(),
   };
 
