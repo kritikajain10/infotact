715385256ae01ec6498cdfad0dfb78c880c11355
kritikajain10 <kritikajain10@gmail.com>
2026-07-25 23:36:02 +0530

    Add canvas animation using requestAnimationFrame

--- frontend/src/components/CanvasMap.tsx
+++ frontend/src/components/CanvasMap.tsx
@@ -3,29 +3,45 @@ import { useEffect, useRef } from "react";
 export default function CanvasMap() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  
   useEffect(() => {
+    let x = 100;
     const canvas = canvasRef.current;
     if (!canvas) return;
 
+   
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    // Background
-    ctx.fillStyle = "#1e293b";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Title
-    ctx.fillStyle = "white";
-    ctx.font = "20px Arial";
-    ctx.fillText("FleetDash Canvas", 20, 35);
-
-    // Demo vehicle
-    ctx.fillStyle = "#22c55e";
-    ctx.beginPath();
-    ctx.arc(100, 150, 10, 0, Math.PI * 2);
-    ctx.fill();
-  }, []);
-
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+      // Background
+      ctx.fillStyle = "#1e293b";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+      // Title
+      ctx.fillStyle = "white";
+      ctx.font = "20px Arial";
+      ctx.fillText("FleetDash Canvas", 20, 35);
+    
+      // Vehicle
+      ctx.fillStyle = "#22c55e";
+      ctx.beginPath();
+      ctx.arc(x, 150, 10, 0, Math.PI * 2);
+      ctx.fill();
+    
+      x += 2;
+    
+      if (x > canvas.width) {
+        x = 0;
+      }
+    
+      requestAnimationFrame(draw);
+    }
+    
+    draw();
+  },[]);
   return (
     <canvas
       ref={canvasRef}
